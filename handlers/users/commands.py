4838c4a823d57ddea90118b6f1ada5f8a7d60af4
Akhmedov76 <ahmadovj5016@gmail.com>
2025-01-05 12:36:02 +0500

    Update all func translation for commands.py

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -10,10 +10,10 @@ router = Router()
 
 
 # Command handler to start feedback collection
-@router.message(Command("feedback"))
+@router.message(Command(_("Fikr-mulohaza")))
 async def feedback(message: types.Message, state: FSMContext):
     admins_id = ADMINS
-    await message.answer(_("Please enter your feedback 👇"))
+    await message.answer(_("Iltimos, fikr-mulohazangizni kiriting 👇"))
 
     state_data = await state.get_data()
     state_data["admins_id"] = admins_id
@@ -25,17 +25,17 @@ async def feedback(message: types.Message, state: FSMContext):
 # Handler to get the feedback and send it to admins
 @router.message(StateFilter(FeedbackState.get_feedback))  # Use StateFilter here
 async def get_feedback(message: types.Message, state: FSMContext):
-    message_data = f'''
-User 👥: {message.from_user.full_name}
+    message_data = _(f'''
+Foydalanuvchi 👥: {message.from_user.full_name}
 ID: {message.from_user.id}
-Feedback: {message.text}
-    '''
+Fikr-mulohaza: {message.text}
+    ''')
     state_data = await state.get_data()
     admins_id = state_data.get("admins_id")
 
     for admin_id in admins_id:
         await message.bot.send_message(admin_id, message_data)
 
-    await message.answer(_("Feedback sent to admins successfully. Thank you for your feedback! 😊"))
+    await message.answer(_("Fikr-mulohaza adminlarga muvaffaqiyatli yuborildi. Fikr-mulohazangiz uchun rahmat! 😊"))
 
     await state.clear()
