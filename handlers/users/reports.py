48317df35e640435631b41c9d786a503709df8af
Asadbek A'lamov <alamovasad@gmail.com>
2025-01-09 14:26:10 +0500

    change

--- handlers/users/reports.py
+++ handlers/users/reports.py
@@ -2,6 +2,10 @@ from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
 
 from keyboards.default.report_kb import report_main_kb
+from keyboards.default.user import user_main_menu_keyboard
+from main.constants import ReportType
+from utils.db_commands.user import get_user_income_and_expense_reports
+from utils.main_functions import create_report
 
 router = Router()
 
@@ -9,3 +13,15 @@ router = Router()
 @router.message(F.text.in_(['Reports 📄', 'Отчеты 📄', 'Hisobotlar 📄']))
 async def branches_handler(message: types.Message, state: FSMContext):
     await message.answer("Hisobot turini tanlang 👇", reply_markup=await report_main_kb())
+
+
+@router.message(F.text.in_(['Umumiy hisobot 📊', 'Umumiy hisobot 📊', 'Umumiy hisobot 📊']))
+async def all_reports_handler(message: types.Message, state: FSMContext):
+    all_incomes: any or list = await get_user_income_and_expense_reports(chat_id=message.chat.id,
+                                                                         report_type=ReportType.income.value)
+    if not all_incomes:
+        await message.answer(_("Sizda daromad bo'yicha hisobot yo'q!"), reply_markup=await user_main_menu_keyboard())
+        return
+    income_report = create_report(data=all_incomes)
+
+    await message.reply(income_report['report_text'], parse_mode='HTML', reply_markup=await user_main_menu_keyboard())
