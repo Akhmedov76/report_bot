60e2d31754e95f4f15cb8885afc11a826ced224d
Asadbek A'lamov <alamovasad@gmail.com>
2025-01-09 14:44:08 +0500

    change

--- handlers/users/reports.py
+++ handlers/users/reports.py
@@ -5,7 +5,7 @@ from keyboards.default.report_kb import report_main_kb
 from keyboards.default.user import user_main_menu_keyboard
 from main.constants import ReportType
 from utils.db_commands.user import get_user_income_and_expense_reports
-from utils.main_functions import create_report
+from utils.main_functions import create_report, create_global_report
 from loader import _
 
 router = Router()
@@ -18,13 +18,12 @@ async def branches_handler(message: types.Message, state: FSMContext):
 
 @router.message(F.text.in_(['Umumiy hisobot 📊', 'Umumiy hisobot 📊', 'Umumiy hisobot 📊']))
 async def all_reports_handler(message: types.Message, state: FSMContext):
-    all_incomes: any or list = await get_user_income_and_expense_reports(chat_id=message.chat.id,
-                                                                         report_type=ReportType.income.value)
+    all_incomes: any or list = await get_user_income_and_expense_reports(chat_id=message.chat.id)
     if not all_incomes:
         await message.answer(_("Hisobot tayyorlash uchun malumot topilmadi! 😔"),
                              reply_markup=await user_main_menu_keyboard())
         await state.clear()
         return
-    income_report = create_report(data=all_incomes)
+    income_report = create_global_report(data=all_incomes)
 
     await message.reply(income_report['report_text'], parse_mode='HTML', reply_markup=await user_main_menu_keyboard())
--- utils/main_functions.py
+++ utils/main_functions.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from main.constants import ReportType
 import pytz
 
 
@@ -22,6 +22,28 @@ def create_report(data):
     report_summ = "{:,}".format(report_summ).replace(",", " ")
     report_text += f"✅ Umumiy hisob: {report_summ} so'm"
 
+    return_data = {
+        "report_text": report_text,
+        "report_summ": report_summ
+    }
+    return return_data
+
+
+def create_global_report(data):
+    report_text = str()
+    report_summ = 0
+    report_text += f"\n 📝 Jami hisobotlar soni: {len(data)} ta\n\n"
+    for index, report in enumerate(data):
+        new_amount = f"{int(report['amount']):,}".replace(",", " ")
+        created_at = change_utc_to_local(str(report['created_at']))
+        if report['type'] == ReportType.income.value:
+            report_summ += report['amount']
+            report_text += f"{index + 1}. Daromad, {str(new_amount)} so'm, {report['description']}, {created_at}" + "\n \n"
+        elif report['type'] == ReportType.expense.value:
+            report_text += f"{index + 1}. Xarajat, {str(new_amount)} so'm, {report['description']}, {created_at}" + "\n \n"
+            report_summ -= report['amount']
+    report_summ = "{:,}".format(report_summ).replace(",", " ")
+    report_text += f"✅ Umumiy hisob: {report_summ} so'm"
     return_data = {
         "report_text": report_text,
         "report_summ": report_summ
