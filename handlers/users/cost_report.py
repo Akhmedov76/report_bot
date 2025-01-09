139691a8e1eda6d451eee241cd3dcb2624daae60
Asadbek A'lamov <alamovasad@gmail.com>
2025-01-09 10:51:25 +0500

    change

--- handlers/users/cost_report.py
+++ handlers/users/cost_report.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
 
+from keyboards.default.report_kb import report_date_kb
 from loader import _
 from main.constants import ReportType
 from keyboards.default.user import user_main_menu_keyboard
@@ -13,12 +14,13 @@ router = Router()
 @router.message(
     F.text.in_(["Xarajatlar bo'yicha hisobot📉", "Xarajatlar bo'yicha hisobot📉", "Xarajatlar bo'yicha hisobot📉"]))
 async def branches_handler(message: types.Message, state: FSMContext):
-    await message.answer(_("Xarajatlar bo'yicha hisobot tayyorlanmoqda, iltimos kuting! 😊"))
-    all_costs: any or list = await get_user_income_and_expense_reports(chat_id=message.chat.id,
-                                                                       report_type=ReportType.expense.value)
-    if not all_costs:
-        await message.answer(_("Sizning xarajatlaringiz hali bo'lmagan!"), reply_markup=await user_main_menu_keyboard())
-        return
-    cost_report = create_report(data=all_costs)
-
-    await message.reply(cost_report['report_text'], reply_markup=await user_main_menu_keyboard())
+    await message.answer(_("Hisobot davomiyligini tanlang 😊 "),
+                         reply_markup=await report_date_kb())
+    # all_costs: any or list = await get_user_income_and_expense_reports(chat_id=message.chat.id,
+    #                                                                    report_type=ReportType.expense.value)
+    # if not all_costs:
+    #     await message.answer(_("Sizning xarajatlaringiz hali bo'lmagan!"), reply_markup=await user_main_menu_keyboard())
+    #     return
+    # cost_report = create_report(data=all_costs)
+    #
+    # await message.reply(cost_report['report_text'], reply_markup=await user_main_menu_keyboard())
