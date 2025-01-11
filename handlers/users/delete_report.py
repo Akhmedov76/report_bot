8391dd7fec2a69ebfbc0f64b81d2483cd838ebcf
Asadbek A'lamov <alamovasad@gmail.com>
2025-01-11 11:33:43 +0500

    change

--- handlers/users/delete_report.py
+++ handlers/users/delete_report.py
@@ -15,11 +15,13 @@ router = Router()
 
 @router.callback_query(lambda c: c.data.startswith("report_page_"))
 async def delete_report_handler(callback_query: CallbackQuery, state: FSMContext):
+    """
+    This handler is called when the user clicks on the "Delete" button in the report page.
+    It deletes the report from the database and returns the user to the main page.
+    :param callback_query: CallbackQuery, the callback query that was clicked.
+    :param state: FSMContext
+    :return: None
+    """
     await callback_query.answer()
-    # Extract the report ID from the callback data
     report_id = callback_query.data.split("_")[2]  # Get the ID from callback_data
-
-    print(report_id)  # This will print the report ID to the console (for debugging purposes)
-
-    # Send a message back to the user with the selected report ID
     await callback_query.message.answer(f"You selected report ID: {report_id}")
