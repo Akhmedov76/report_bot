22f1240ebef0842f047b3aafcf40ab74e9996168
Asadbek A'lamov <alamovasad@gmail.com>
2025-01-17 18:17:17 +0500

    change

--- main/database.py
+++ main/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from main.config import DB_USER, DB_NAME, DB_HOST, DB_PASS, DB_PORT
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 database = databases.Database(DATABASE_URL)
 
 Base = declarative_base()
